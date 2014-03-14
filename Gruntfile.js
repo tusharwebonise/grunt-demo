a2635dad0be67a06dc2a9899cf2f414bfbb2f401
Tushar Bandal <tushar@weboniselab.com>
2014-03-14 12:14:17 +0530

    first push

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+﻿module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {   
+            dist: {
+                src: [
+                    'js/jquery-1.10.2.min.js',
+                    'js/jquery.fancybox.js'
+                ],
+                dest: 'js/build/production.js',
+                nonull: true
+            }
+        },
+
+        uglify: {
+            options: {
+              banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        compass: {
+            dist: {
+                options: {
+                sassDir: 'sass',
+                cssDir: 'css'
+                }
+            }
+        },
+        cssmin: {
+          add_banner: {
+            options: {
+              banner: '/* My minified css file */'
+            },
+            files: {
+              'css/production.min.css': [
+                'css/bootstrap.css',
+                'css/jquery.fancybox.css',
+                'css/custom.css'
+              ]
+            }
+          }
+        },
+
+
+        watch: {
+            gruntfile: {
+              files: 'Gruntfile.js',
+              tasks: ['notify:gruntChange']
+            },
+            scripts: {
+                files: ['js/*.js', 'js/libs/*.js'],
+                tasks: ['concat', 'uglify']
+            },
+            csstosass: {
+                files: ['sass/*.sass'],
+                tasks: ['compass']
+            },
+            css: {
+                files: ['css/*.css'],
+                tasks: ['cssmin']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['concat','uglify', 'compass', 'cssmin' ,'watch']);
+};
